7067d768736444a987966ade29b4dde456bc0b7b
Marc Vial <marc.vial@hes-so.ch>
2020-04-30 13:41:31 +0200

    Test

---
+++ scripts/canvas.js
@@ -0,0 +1,13 @@
+/*
+var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    widht = window.innerWidth,
+    height = window.innerHeight;
+
+canvas.width = widht;
+canvas.height = height;
+
+
+context.fillRect(10,20,100,200);
+*/
+
